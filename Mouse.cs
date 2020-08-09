4cc2b77643f463d7e407cb7a71fb6cc3398f2204
LemonBruh <63004992+LemonBruh@users.noreply.github.com>
2020-08-09 17:07:44 +0100

    Add files via upload

---
+++ Mouse.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Mouse : MonoBehaviour
+{
+    public void Showmouse()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+    }
+
+    public void Hidemouse()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
