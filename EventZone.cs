fff94349b01adecadf800c37facd180123b2beee
LemonBruh <63004992+LemonBruh@users.noreply.github.com>
2020-08-21 17:10:22 +0100

    Add files via upload

---
+++ EventZone.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventZone : MonoBehaviour
+{
+    
+        public UnityEngine.Events.UnityEvent onTriggerEnterEvent;
+        void OnTriggerEnter(Collider other)
+        {
+            onTriggerEnterEvent.Invoke();
+    }
+}
