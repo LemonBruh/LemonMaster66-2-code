64348a9ebc5ba4970d7b342c007903d5f3fd25f6
LemonBruh <63004992+LemonBruh@users.noreply.github.com>
2021-02-06 00:57:05 +0000

    Add files via upload

---
+++ SceneLoader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour 
+{
+    public void changemenuscene(string levelNumber)
+	{
+        Application.LoadLevel(levelNumber);
+    }
+}
