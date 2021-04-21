e72645770535d7b206d48efd0dd16d62e469f778
LemonBruh <63004992+LemonBruh@users.noreply.github.com>
2021-04-21 19:03:27 +0100

    Add files via upload

---
+++ CustomKeyCodeEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CustomKeyCodeEvent : MonoBehaviour
+{
+    [SerializeField] String _keyCodeString;
+    [SerializeField] UnityEvent _event;
+    KeyCode _keyCode;
+
+    void Awake()
+    {
+        _keyCode = (KeyCode)Enum.Parse(typeof(KeyCode), _keyCodeString, true);
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(_keyCode))
+        {
+            Debug.Log(_keyCode + " key was pressed");
+            _event?.Invoke();
+        }
+    }
+
+    public void DemoEvent()
+    {
+        Debug.Log("Demo event was triggered");
+    }
+}
---
+++ KeyCodeEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KeyCodeEvent : MonoBehaviour
+{
+    [SerializeField] KeyCode _keyCode;
+    [SerializeField] UnityEvent _event;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(_keyCode))
+        {
+            Debug.Log(_keyCode + " pressed");
+            _event?.Invoke();
+        }
+    }
+
+    public void DemoEvent()
+    {
+        Debug.Log("Demo event was triggered");
+    }
+}
