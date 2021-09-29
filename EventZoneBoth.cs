f62d1c68b691114f82e639c01b8180d34761df2a
LemonBruh <63004992+LemonBruh@users.noreply.github.com>
2021-09-29 17:06:50 +0100

    Tutorial series
    event zone part 2

---
+++ EventZoneBoth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventZoneBoth : MonoBehaviour
+{
+
+    public UnityEngine.Events.UnityEvent onTriggerEnter;
+    void OnTriggerEnter(Collider other)
+    {
+        onTriggerEnter.Invoke();
+    }
+    public UnityEngine.Events.UnityEvent onTriggerExit;
+    void OnTriggerExit(Collider other)
+    {
+        onTriggerExit.Invoke();
+    }
+}
---
+++ EventZoneExit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventZoneExit : MonoBehaviour
+{
+    
+        public UnityEngine.Events.UnityEvent onTriggerEnterExit;
+        void OnTriggerExit(Collider other)
+        {
+            onTriggerEnterExit.Invoke();
+    }
+}
