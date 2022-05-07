c943a1a11e5f6f3fbb1f4b165155f0433d82afff
LemonBruh <63004992+LemonBruh@users.noreply.github.com>
2022-05-07 17:08:33 +0100

    key code stuff

---
+++ KeyCodeBoth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KeyCodeBoth : MonoBehaviour
+{
+    [SerializeField] KeyCode _keyCode;
+    [SerializeField] UnityEvent _keyDown;
+    [SerializeField] UnityEvent _keyUp;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(_keyCode))
+        {
+            Debug.Log(_keyCode + " pressed");
+            _keyDown?.Invoke();
+        }
+
+        if (Input.GetKeyUp(_keyCode))
+        {
+            Debug.Log(_keyCode + " Un-Pressed");
+            _keyUp?.Invoke();
+        }
+    }
+
+    public void DemoEvent()
+    {
+        Debug.Log("Demo event was triggered");
+    }
+}
---
+++ KeyCodeUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KeyCodeUp : MonoBehaviour
+{
+    [SerializeField] KeyCode _keyCode;
+    [SerializeField] UnityEvent _event;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(_keyCode))
+        {
+            Debug.Log(_keyCode + " Not Pressed");
+            _event?.Invoke();
+        }
+    }
+
+    public void DemoEvent()
+    {
+        Debug.Log("Demo event was triggered");
+    }
+}
